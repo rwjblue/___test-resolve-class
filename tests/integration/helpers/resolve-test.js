844b8eb8f0391d54ff4bf2a28233f3c77bd619eb
Robert Jackson <rjackson@linkedin.com>
2022-11-09 16:59:00 -0500

    Add tests for helpers

--- tests/integration/helpers/resolve-test.js
+++ tests/integration/helpers/resolve-test.js
@@ -2,13 +2,14 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { helper } from '@ember/component/helper';
 import Component from '@glimmer/component';
 import { setComponentTemplate } from '@ember/component';
 
 module('Integration | Helper | resolve', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders components with pre-associated templates (e.g. colocated)', async function (assert) {
+  test('it renders components with pre-associated templates (e.g. colocated) using {{let}}', async function (assert) {
     class Foo extends Component {}
     setComponentTemplate(hbs`Hello!`, Foo);
     this.owner.register('component:foo', Foo);
@@ -16,5 +17,15 @@ module('Integration | Helper | resolve', function (hooks) {
     await render(hbs`{{#let (resolve type="component" name="foo") as |Comp|}}<Comp />{{/let}}`);
 
     assert.dom(this.element).hasText('Hello!');
+  });
+
+  test('it invokes helpers', async function (assert) {
+    this.owner.register('helper:foo', helper(() => {
+      return false;
+    }));
+
+    await render(hbs`{{#if (resolve type="helper" name="foo")}}returned false!{{/if}}`);
+
+    assert.dom(this.element).hasText('returned false!');
   });
 });
